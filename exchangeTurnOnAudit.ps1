73cc962bd0ad22014e95e93d5b5be47516f6b2a7
Joe Stine <thejoeshow24@gmail.com>
2018-10-22 16:48:03 -0700

    Update exchangeTurnOnAudit.ps1

---
+++ exchangeTurnOnAudit.ps1
@@ -0,0 +1,6 @@
+﻿$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+
+Connect-MsolService –Credential $LiveCred
+
+Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true
