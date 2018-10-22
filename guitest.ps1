07dcda57ceb13c35e47076261b01090b7d66de5a
Joe Stine <thejoeshow24@gmail.com>
2018-10-22 16:48:15 -0700

    Update guitest.ps1

---
+++ guitest.ps1
@@ -0,0 +1,57 @@
+﻿<# This form was created using POSHGUI.com  a free online gui designer for PowerShell
+.NAME
+    Untitled
+#>
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+#region begin GUI{ 
+
+$Form                            = New-Object system.Windows.Forms.Form
+$Form.ClientSize                 = '400,400'
+$Form.text                       = "Form"
+$Form.TopMost                    = $false
+
+$Button1                         = New-Object system.Windows.Forms.Button
+$Button1.text                    = "button"
+$Button1.width                   = 60
+$Button1.height                  = 30
+$Button1.location                = New-Object System.Drawing.Point(52,286)
+$Button1.Font                    = 'Microsoft Sans Serif,10'
+
+$Button2                         = New-Object system.Windows.Forms.Button
+$Button2.text                    = "button"
+$Button2.width                   = 60
+$Button2.height                  = 30
+$Button2.location                = New-Object System.Drawing.Point(214,283)
+$Button2.Font                    = 'Microsoft Sans Serif,10'
+
+$TextBox1                        = New-Object system.Windows.Forms.TextBox
+$TextBox1.multiline              = $false
+$TextBox1.width                  = 100
+$TextBox1.height                 = 20
+$TextBox1.location               = New-Object System.Drawing.Point(47,108)
+$TextBox1.Font                   = 'Microsoft Sans Serif,10'
+
+$Label1                          = New-Object system.Windows.Forms.Label
+$Label1.text                     = "label"
+$Label1.AutoSize                 = $true
+$Label1.width                    = 25
+$Label1.height                   = 10
+$Label1.location                 = New-Object System.Drawing.Point(53,55)
+$Label1.Font                     = 'Microsoft Sans Serif,10'
+
+$Form.controls.AddRange(@($Button1,$Button2,$TextBox1,$Label1))
+
+#region gui events {
+$Button1.Add_Click({  })
+$Button2.Add_Click({  })
+#endregion events }
+
+#endregion GUI }
+
+
+#Write your logic code here
+
+[void]$Form.ShowDialog()
