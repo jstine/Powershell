482f08700ac3a0b84cbc4da121a52be7c666312d
Joe Stine <thejoeshow24@gmail.com>
2018-10-22 16:48:17 -0700

    Update Hotfixreport.ps1

---
+++ Hotfixreport.ps1
@@ -0,0 +1,18 @@
+﻿function Hotfixreport {
+$computers = Get-Content $env:USERPROFILE\Desktop\Computers.txt
+$ErrorActionPreference = 'Stop'
+ForEach ($computer in $computers) {
+    try{
+        Get-HotFix -ComputerName $computer | Select-Object PSComputerName,HotFixID,Description,InstalledBy,InstalledOn -Last 20 | ConvertTo-html
+    }
+
+    catch{
+        Add-Content $computer -Path "$env:USERPROFILE\Desktop\NotReachable_Servers.txt"
+    }
+}
+}
+
+Hotfixreport > "E:\users\joe\Desktop\HotFixReport.htm"
+
+
+
