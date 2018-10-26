64c5c77a81a14b0a1b7eb80f285fd36ce7c2145a
jstine <thejoeshow24@gmail.com>
2018-10-25 23:21:42 -0700

    Update MenuTest.ps1

---
+++ MenuTest.ps1
@@ -0,0 +1,55 @@
+﻿function Show-Menu
+{
+    param (
+        [string]$Title = 'My Menu'
+    )
+
+
+Clear-Host
+Write-Host "=======$Title======="
+
+Write-Host "1: Press '1' for this option."
+Write-Host "2: Press '2' for this option."
+Write-Host "3: Press '3' for this option."
+Write-Host "Q: Press 'Q' to quit." 
+}
+
+
+do
+{
+Show-Menu -Title 'My Menu'
+$Selection = Read-Host "Please make a selection."
+switch ($Selection)
+{
+    '1' {'You chose option #1'} 
+    '2' {'You chose option #2'}
+    '3' {'You chose option #3'} 
+ }
+ pause
+ }
+ until ($Selection -eq 'q')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
