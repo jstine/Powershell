196116f0f7a4f09a9d3888aed74b21e88c5e7461
jstine <thejoeshow24@gmail.com>
2018-10-29 23:44:06 -0700

    Update FileCopier.ps1

---
+++ FileCopier.ps1
@@ -0,0 +1,17 @@
+﻿$ArchivePath = Read-Host "Archive Path"
+$TargetPath = Read-Host "Target Path"
+$MovedFiles = Read-Host "Log Path"
+$ErrorLog = Read-Host "Error Log Path"
+
+Get-ChildItem $TargetPath | ForEach-Object {
+$FilePath = $_.fullname
+Try
+{
+Move-Item $FilePath $ArchivePath
+$FilePath | Add-Content $MovedFiles
+}
+Catch
+{
+"ERROR moving $filename: $_" | Add-Content $ErrorLog
+}
+}
